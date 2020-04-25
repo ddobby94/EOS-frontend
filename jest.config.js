0a7a91dc39242aa287c0e56ad9e4d9c0edecd7f3
Dobiász Dávid <dobiaszdavid@dobiaszs-mbp.t.hu>
2020-04-25 16:09:54 +0200

    fix Jest + npm start

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(test).ts?(x)", "**/?(*.)+(spec).ts?(x)"]
-};
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.js$': "babel-jest",
+        '^.+\\.tsx?$': 'ts-jest',
+        ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform"
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx|js|jsx|ts)?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    setupFiles: [
+        "raf/polyfill",
+        "./tools/enzymeTestAdapterSetup.js"
+    ],
+    transformIgnorePatterns: [
+        "/node_modules/",
+        "^.+\\.module\\.(css|sass|scss)$"
+    ],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tools/assetsTransformer.js",
+        "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+    }
+}
