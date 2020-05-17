19eedde207de7c778539a6f28f1a40c45e8ef3b1
Dobiász Dávid <dobiaszdavid@192.168.1.14>
2020-05-17 22:10:15 +0200

    add babel-jest for scss moduleNameMapping

--- jest.config.js
+++ jest.config.js
@@ -17,6 +17,6 @@ module.exports = {
     ],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tools/assetsTransformer.js",
-        "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+        "^.+\\.module\\.(css|sass|scss)$": "babel-jest"
     }
 }
--- src/containers/LoginPage.tsx
+++ src/containers/LoginPage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getUser, getAuthLoading } from '../redux/reducers/authReducer';
 import { fetchAuthData } from '../redux/actions/authActions';
