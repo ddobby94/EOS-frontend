6111173cc537e5d8bffbf5ae5196fd5dbdea9249
Dobiász Dávid <dobiaszdavid@192.168.1.14>
2020-05-17 22:16:31 +0200

    use styleMocks

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,7 @@ module.exports = {
     roots: ['<rootDir>/src'],
     transform: {
         '^.+\\.js$': "babel-jest",
-        '^.+\\.tsx?$': 'ts-jest',
-        ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform"
+        '^.+\\.tsx?$': 'ts-jest'
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx|js|jsx|ts)?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
@@ -17,6 +16,6 @@ module.exports = {
     ],
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/tools/assetsTransformer.js",
-        "^.+\\.module\\.(css|sass|scss)$": "babel-jest"
+         "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.js"
     }
 }
