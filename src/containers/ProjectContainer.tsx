8d389d0dbc1c94a08324f125270ec28080e7040b
David Dobiasz <david.dobiasz@blackrock.com>
2020-05-19 11:32:38 +0200

    define Header and footer in projectContainer

--- src/containers/ProjectContainer.tsx
+++ src/containers/ProjectContainer.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Icon, Button } from '@material-ui/core';
+import { Icon, Fab, Button } from '@material-ui/core';
 import { ContentCard } from '../components/common/ContentCard';
 import { ProgressBar } from '../components/ProgressBar';
+import './styles/project.scss';
 
 const PROGRESS_BAR_ELEMENTS = ['Import', 'Exploratory Analysis', 'Bivariate Analysis', 'pre-processing', 'Model development'];
 
 const ProjectHeader = ({ projectTitle }) => (
     <div className="project-headerContainer">
-        <h2>EOS</h2>
+        <h2 className="project-logo">EOS</h2>
         <h2>{projectTitle}</h2>
-        <Icon
-            className="fa fa-times"
-        />
+        <Fab className="project-closeButton" size="medium" aria-label="add">
+            <Icon
+                className="fa fa-times"
+            />
+        </Fab>
     </div>
 );
 
@@ -30,13 +33,23 @@ export const ProjectContainer = () => {
                 </div>
             </ContentCard>
             <div className="project-footerContainer">
-                <Button>PREVIOUS</Button>
-                <ProgressBar
+                <Button
+                    className="project-previoustButton"
+                    variant="contained"
+                >PREVIOUS</Button>
+                <div className="project-progressBar">
+                    <ProgressBar
                     items={PROGRESS_BAR_ELEMENTS}
                     active={1}
                     onChange={console.log}
-                />
-                <Button>NEXT</Button>
+                    />
+                </div>
+
+                <Button
+                    color="primary"
+                    variant="contained"
+                >NEXT</Button>
+
             </div>
         </div>
     );
